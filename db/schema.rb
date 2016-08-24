5b3aa54189b52823b97ab75a84080f2edf4ec813
Bibi Khadiza <limakhadiza@yahoo.com>
2016-08-24 14:41:41 -0400

    done

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,7 @@
 ActiveRecord::Schema.define(version: 3) do
 
   create_table "students", force: :cascade do |t|
-    t.string  "name"
-    t.integer "grade"
-    t.string  "birthdate"
+    t.string "name"
   end
 
 end
